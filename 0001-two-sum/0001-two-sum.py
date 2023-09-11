eb508552b9cceb7fedc9706d3e0b12c3ae02a8d6
MerlynCoding <144709971+MerlynCoding@users.noreply.github.com>
2023-09-12 00:21:04 +0800

    Time: 364 ms (52.69%), Space: 13.7 MB (99.63%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        answer=[]
+        for i in range(len(nums)):
+            ranged=target-nums[i]
+            if ranged in nums and nums.index(ranged)!=i :
+                
+                answer+=[i,nums.index(ranged)]
+                return sorted(answer)
+                break
