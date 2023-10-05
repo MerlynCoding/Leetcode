e2351dff8a3cc579a53a9107a4f32c2f2d983b1d
MerlynCoding <144709971+MerlynCoding@users.noreply.github.com>
2023-10-05 23:53:11 +0800

    Time: 25 ms (31.31%), Space: 13 MB (93.14%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,6 +1,19 @@
 class Solution(object):
     def mySqrt(self, x):
-        import math as m 
-        if x>=0:
-            return int(m.sqrt(x))
+        if x <2:
+            return x
+        
+        a = 0
+        b = x
+        while abs(a-b) != 1:
+            mid = (a+b) // 2
+            if mid*mid == x:
+                return mid
+
+            if mid*mid > x:
+                b = mid
+            else:
+                a = mid
+        
+        return a
         
