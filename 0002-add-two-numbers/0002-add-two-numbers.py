6da60bf2f31765edad08e82496c5172d43df5939
MerlynCoding <144709971+MerlynCoding@users.noreply.github.com>
2023-10-19 23:22:53 +0800

    Time: 42 ms (70.39%), Space: 13.3 MB (42%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -3,23 +3,27 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        dummy=ListNode(0)
-        total=dummy
-        front=0
-        back=0
-        while l1 or l2 or back:
-            l1_val=l1.val if l1 else 0
-            l2_val=l2.val if l2 else 0
-            plus=l1_val+l2_val+back
-            front=plus%10
-            new_node=ListNode(front)
-            total.next=new_node
-            total=total.next
-            back=plus//10
-            l1=l1.next if l1 else None
-            l2=l2.next if l2 else None
-        return dummy.next
+class Solution:
+    def addTwoNumbers(self, l1,l2):
+        dummyHead = ListNode(0)
+        tail = dummyHead
+        carry = 0
 
-        
+        while l1 is not None or l2 is not None or carry != 0:
+            digit1 = l1.val if l1 is not None else 0
+            digit2 = l2.val if l2 is not None else 0
+
+            sum = digit1 + digit2 + carry
+            digit = sum % 10
+            carry = sum // 10
+
+            newNode = ListNode(digit)
+            tail.next = newNode
+            tail = tail.next
+
+            l1 = l1.next if l1 is not None else None
+            l2 = l2.next if l2 is not None else None
+
+        result = dummyHead.next
+        dummyHead.next = None
+        return result
